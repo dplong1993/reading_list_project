86c2dd18abd1ac133d65f1adbc80c9c659136d9b
Dillon Long <dplong@crimson.ua.edu>
2020-07-30 10:54:40 -0700

    Implemented the edit-book page and refactored code in some of the view files

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const csrf = require('csurf');
+const { check, validationResult } = require('express-validator');
 
 const db = require('./db/models');
 
@@ -24,34 +25,113 @@ router.get('/book/add', csrfProtection, (req, res) => {
   });
 });
 
-router.post('/book/add', csrfProtection, asyncHandler(async (req, res) => {
-  const {
-    title,
-    author,
-    releaseDate,
-    pageCount,
-    publisher,
-  } = req.body;
+const bookValidators = [
+  check('title')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Title')
+    .isLength({ max: 255 })
+    .withMessage('Title must not be more than 255 characters long'),
+  check('author')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Author')
+    .isLength({ max: 100 })
+    .withMessage('Author must not be more than 100 characters long'),
+  check('releaseDate')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Release Date')
+    .isISO8601()
+    .withMessage('Please provide a valid date for Release Date'),
+  check('pageCount')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Page Count')
+    .isInt({ min: 0 })
+    .withMessage('Please provide a valid integer for Page Count'),
+  check('publisher')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Publisher')
+    .isLength({ max: 100 })
+    .withMessage('Publisher must not be more than 100 characters long'),
+];
 
-  const book = db.Book.build({
-    title,
-    author,
-    releaseDate,
-    pageCount,
-    publisher,
-  });
+router.post('/book/add', csrfProtection, bookValidators,
+  asyncHandler(async (req, res) => {
+    const {
+      title,
+      author,
+      releaseDate,
+      pageCount,
+      publisher,
+    } = req.body;
 
-  try {
-    await book.save();
-    res.redirect('/');
-  } catch (err) {
-    res.render('book-add', {
-      title: 'Add Book',
+    const book = db.Book.build({
+      title,
+      author,
+      releaseDate,
+      pageCount,
+      publisher,
+    });
+
+    const validatorErrors = validationResult(req);
+
+    if (validatorErrors.isEmpty()) {
+      await book.save();
+      res.redirect('/');
+    } else {
+      const errors = validatorErrors.array().map((error) => error.msg);
+      res.render('book-add', {
+        title: 'Add Book',
+        book,
+        errors,
+        csrfToken: req.csrfToken(),
+      });
+    }
+  }));
+
+router.get('/book/edit/:id(\\d+)', csrfProtection,
+  asyncHandler(async (req, res) => {
+    const bookId = parseInt(req.params.id, 10);
+    const book = await db.Book.findByPk(bookId);
+    res.render('book-edit', {
+      title: 'Edit Book',
       book,
-      error: err,
       csrfToken: req.csrfToken(),
     });
-  }
-}));
+  }));
 
+router.post('/book/edit/:id(\\d+)', csrfProtection, bookValidators,
+  asyncHandler(async (req, res) => {
+    const bookId = parseInt(req.params.id, 10);
+    const bookToUpdate = await db.Book.findByPk(bookId);
+
+    const {
+      title,
+      author,
+      releaseDate,
+      pageCount,
+      publisher,
+    } = req.body;
+
+    const book = {
+      title,
+      author,
+      releaseDate,
+      pageCount,
+      publisher,
+    };
+
+    const validatorErrors = validationResult(req);
+
+    if (validatorErrors.isEmpty()) {
+      await bookToUpdate.update(book);
+      res.redirect('/');
+    } else {
+      const errors = validatorErrors.array().map((error) => error.msg);
+      res.render('book-edit', {
+        title: 'Edit Book',
+        book: { ...book, id: bookId },
+        errors,
+        csrfToken: req.csrfToken(),
+      });
+    }
+  }));
 module.exports = router;
